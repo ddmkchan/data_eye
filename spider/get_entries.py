4de7e4c47095e7dd59b9157098e678d6c171a856
ddmkchan@163.com <ddmkchan@163.com>
2015-06-16 18:16:13 +0800

    fix

--- spider/get_entries.py
+++ spider/get_entries.py
@@ -13,16 +13,37 @@ names = "爸爸回来了,爸爸去哪儿"
 
 url = "http://so.iqiyi.com/so/q_%s"
 
-
 def func():
+	URLS = [(i, url %i) for i in names.split(',')]
+	for i in URLS:
+		r = s.get(i[1])
+		soup = BeautifulSoup(r.text)
+		for link in soup.find_all("a", class_="album_link"):
+			if 'javascript' not in link.get('href'):
+				#href = link.get('href')
+				#m = re.search('data-player-tvid=\"(\d+)\"', s.get(href).text)
+				#tvid = m.group(1) if m is not None else ''
+				#m2 = re.search('data-qitancomment-qitanid=\"(\d+)\"', s.get(href).text)
+				#qitanid = m2.group(1) if m2 is not None else ''
+				#if tvid and qitanid:
+				#	print i[0], link.get('title').encode('utf-8'), link.get('href').encode('utf-8'), tvid, qitanid
+				print i[0], link.get('title').encode('utf-8'), link.get('href').encode('utf-8')
+
+
+def func2():
 	URLS = [(i, url %i) for i in tv_names.split(',')]
 	for i in URLS:
 		r = s.get(i[1])
 		soup = BeautifulSoup(r.text)
 		for link in soup.find_all("a", class_="album_link"):
 			if 'javascript' not in link.get('href'):
-				print i[0], link.get('href').encode('utf-8')
-			
+				href = link.get('href')
+				m = re.search('data-player-tvid=\"(\d+)\"', s.get(href).text)
+				tvid = m.group(1) if m is not None else ''
+				m2 = re.search('data-qitancomment-qitanid=\"(\d+)\"', s.get(href).text)
+				qitanid = m2.group(1) if m2 is not None else ''
+				if tvid and qitanid:
+					print i[0], link.get('title').encode('utf-8'), link.get('href').encode('utf-8'), tvid, qitanid
 			
 if __name__ == '__main__':
 	func()
