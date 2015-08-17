e7aa32c9472618e92969793311311f89b5f7bcee
ddmkchan@163.com <ddmkchan@163.com>
2015-08-17 19:46:54 +0800

    add r project

--- spider/get_entries.py
+++ spider/get_entries.py
@@ -85,10 +85,112 @@ def get_baba_comeback_entry():
 			db_conn.add(item)
 	db_conn.commit()
 			
+def get_xiaomi():
+	f = open('xm')
+	soup = BeautifulSoup(f.read().decode('utf-8'))
+	ll = [re.split('\_|\.', i.get('href'))[-2] for i in soup.find_all("a")]
+	print len(ll)
+	#for i in soup.find_all("a"):
+#		href = i.get('href')
+#		print re.split('\_|\.', href)
+
+
+_category_list_360 = [19, 20, 51, 52, 53, 54, 101587, 102238, 100451]
+#_category_list_360 = [11, 12, 14, 15,16, 17,18, 102228, 102230, 102231, 102232, 102233, 102239]
+
+def get_360_list(cid, page):
+	rs = []
+	r = s.get('http://zhushou.360.cn/list/index/cid/%s?page=%s' % (cid, page))
+	soup = BeautifulSoup(r.text)
+	for i in soup.find("ul", class_="iconList").find_all("li"):
+		item = i.find('h3').find('a')
+		rs.append((item.text, item.get('sid')))
+	return rs
+
+def get_360_tags(sid):
+	r = s.get('http://zhushou.360.cn/detail/index/soft_id/%s' % sid)
+	soup = BeautifulSoup(r.text)
+	tags = soup.find("div", class_="app-tags")
+	if tags is not None:
+		return u",".join([i.text for i in tags.find_all("a")])
+	return u""
+
+def get_360_main():
+	count = 0
+	for c in _category_list_360:
+		category = gameid2category.get(str(c))
+		#category = id2category.get(str(c))
+		for i in xrange(1, 51):
+			icon_list = get_360_list(c, i)
+			#print category, ",".join([i[0] for i in icon_list])
+			for icon in icon_list:
+				#print "%s\t%s\t%s" % (icon[1], icon[0].encode('utf-8'), category)
+				tags = get_360_tags(icon[1])
+				#print icon[0], icon[1], tags
+				count += 1
+				#ins = db_conn.query(APPLIST2).filter(APPLIST2.sid==int(icon[1])).first()
+				#if not ins:
+				item = APPLIST2(**{'sid': int(icon[1]), 'app_name': icon[0], 'tags': tags, 'category':category})
+				db_conn.merge(item)
+				if count % 1000 == 0:
+					print "%s commit" % count
+					db_conn.commit()
+	db_conn.commit()
+
+id2category = {
+"11"	 :"系统安全", 
+"12" 	 :"通讯社交",
+"14"	 :"影音视听",
+"15"	 :"新闻阅读",
+"16"	 :"生活休闲",
+"18" 	 :"主题壁纸",
+"17" 	 :"办公商务",
+"102228" :"摄影摄像",
+"102230" :"购物优惠",
+"102231" :"地图旅游",
+"102232" :"教育学习",
+"102139" :"金融理财",
+"102233" :"健康医疗"}
+
+
+gameid2category = {
+"20": 		"动作冒险", 
+"19": 		"休闲益智",
+"54": 		"棋牌天地",
+"51": 		"体育竞速",
+"53": 		"经营策略",
+"52": 		"飞行射击",
+"100451": 	"网络游戏",
+"102238": 	"儿童游戏",
+"101587": 	"角色扮演"
+}
+
+def match():
+	with open('dataeye_games') as f:
+		for line in f.readlines()[:]:
+			app = line.rstrip()
+			#print "****", app
+			ins = db_conn.query(APPLIST2).filter(APPLIST2.app_name==app).filter(APPLIST2.tags!=u'').first()
+			if ins:
+				if len(ins.tags) != 0:
+					print "%s\t%s" % (app, ins.tags.encode('utf-8'))
+
+
+def get_ip_info():
+	URL = "http://ip.chinaz.com/"
+	payload = {'IP': '058.205.255.255,183.13.153.142,058.206.159.2551'}
+	r = s.get(URL, params=payload)
+	soup = BeautifulSoup(r.text)
+	for i in soup.find("span", id="status").find_all('strong'):
+		segs = i.text.split(": ")[1].split("==>>")
+		
+
+with open('/home/cyp/17_ip/ip_wifi.log') as f:
+	lines = f.readlines()
+	
 
 if __name__ == '__main__':
-	#func2()
-	#get_link_from_file()
-    #func()
-	get_baba_comeback_entry()
-	#process()
+	#get_360_tags(1838349)
+	#get_360_main()
+	#match()
+	get_ip_info()
--- spider/tm.py
+++ spider/tm.py
@@ -20,21 +20,25 @@ def get_word_dict():
 	with open('custom.dict') as f:
 		return [line.rstrip() for line in f.readlines()]
 
+def main():
+	tv = '爸爸去哪儿,爸爸回来了,虎妈猫爸,嘿老头,待嫁老爸,酷爸俏妈'
+	for name in tv.split(','):
+		print name
+		mydict = {}
+		for ret in db_conn.query(LETV_TV_COMMENTS).filter(LETV_TV_COMMENTS.tv_name==name).all():
+			for w in mmseg.Algorithm(ret.comment.encode('utf-8').lower()):
+				w = w.text.decode('utf-8')
+				if len(w) >= 2:
+					mydict[w] = mydict[w]+1 if w in mydict else 1
+		_s = sorted(mydict.iteritems(), key=lambda d:d[1], reverse=True)
+		for i in _s[:100]:
+			if i[0].encode('utf-8') in get_word_dict():
+				print "%s\t%s" % (i[0], i[1])
 
 if __name__ == '__main__':
-	#for i in mmseg.Algorithm('森碟奥莉kimi暂存以备天天提交的变更'):
-	#	print i
-	#mydict = {}
-	#for ret in db_conn.query(IQIYI_TV_COMMENTS).filter(IQIYI_TV_COMMENTS.tv_name=='爸爸回来了').all():
-	#	print "-------", ret.comment
-	#	for k in mmseg.Algorithm(ret.comment.encode('utf-8')):
-	#		print k
-	#	for w in mmseg.Algorithm(ret.comment.encode('utf-8')):
-	#		print w, '****'
-	#		w = w.decode('utf-8')
-	#		if len(w) >= 2:
-	#			mydict[w] = mydict[w]+1 if w in mydict else 1
-	#print mydict
+	#for i in mmseg.Algorithm('森碟嗯哼奥莉kimi暂存以备天天提交的变更'):
+	#		print i
 
-	for w in get_word_dict():
-		print "%s\t%s" %(w, len(w.decode('utf-8')))
+	#for w in get_word_dict():
+	#	print "%s %s" %(len(w.decode('utf-8')), w)
+	main()
