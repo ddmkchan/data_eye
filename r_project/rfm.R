a66f967a322f840f45605899df1109727b7d46e3
ddmkchan@163.com <ddmkchan@163.com>
2015-08-21 17:28:15 +0800

    add r plott

--- r_project/rfm.R
+++ r_project/rfm.R
@@ -6,23 +6,28 @@ tt <- read.delim("/home/cyp/data_eye/r_project/uid_rfm_payment_d30", header=TRUE
 #first_pay <- tt$payamount_d30==tt$payamount
 d <- data.frame(tt[, c(2,12,13)], row.names=tt$uid)
 #标准化处理
-cl <- kmeans(scale(d), clusters, iter.max = 20)
-rs <- data.frame(cl$cluster)
-mytable <- with(rs, table(rs$cl.cluster))
+kc <- kmeans(scale(d), clusters, iter.max = 20)
+rs <- data.frame(kc$cluster)
+mytable <- with(rs, table(rs$kc.cluster))
 mytable
-d <- cbind(d, rs)
+d <- cbind(scale(d), rs)
+#聚类结果可视化 
+plot(d[,2:3], col = kc$cluster, pch = as.integer(d$kc.cluster))
+#不同的颜色代表不同的聚类结果，不同的形状代表训练数据集的原始分类情况。
+points(kc$centers[,2:3], col = 1:10, pch = 8, cex=2)
 paytimes_d30 <- c()
 payamount_d30 <- c()
 date_diff <- c()
 total <- c()
 for (i in 1:clusters) {
-	c <- d[which(d$cl.cluster==i),]
+	c <- d[which(d$kc.cluster==i),]
 	paytimes_d30 <- append(paytimes_d30, round(mean(c$paytimes_d30), digits=0))
 	payamount_d30 <- append(payamount_d30, round(mean(c$payamount_d30), digits=2))
 	date_diff <- append(date_diff, round(mean(c$date_diff), digits=2))
 	total <- append(total, mytable[i])
-	print(paste("cluster ", i, " size ", nrow(c)))
+	print(paste("kcuster ", i, " size ", nrow(c)))
 	print(summary(c))}
 out <- data.frame(paytimes_d30, payamount_d30, date_diff, total)
 print(out)
 write.table(out, "rfm.txt", sep="\t")
+dev.off()
--- r_project/user_payment.R
+++ r_project/user_payment.R
@@ -1,11 +1,20 @@
 #! /usr/bin/env Rscript
 
-clusters <- 8
+clusters <- 5
 tt <- read.delim("/home/cyp/data_eye/r_project/uid_rfm_payment_d30", header=TRUE)
 #tt <- read.delim("/home/cyp/data_eye/r_project/test_set", header=TRUE)
 d <- data.frame(tt[, c(2,10,11,12,13)], row.names=tt$uid)
-d$playtimes_d30 <- round(ifelse(d$logintimes_d30==0, 0, (d$duration_d30 / d$logintimes_d30 / 60)), digits=2)
-d$logintimes_d30 <- NULL
+d$playtimes_d30 <- round(ifelse(d$logintimes_d30==0, 0, (d$duration_d30 / 30 / 60)), digits=2)
+#d$playtimes_d30 <- round(ifelse(d$logintimes_d30==0, 0, (d$duration_d30 / d$logintimes_d30 / 60)), digits=2)
+#summary(d$playtimes_d30)
+#d$level[d$playtimes_d30>=0 & d$playtimes_d30<5] <- 1
+#d$level[d$playtimes_d30>=5 & d$playtimes_d30<10] <- 2
+#d$level[d$playtimes_d30>=10 & d$playtimes_d30<30] <- 3
+#d$level[d$playtimes_d30>=30 & d$playtimes_d30<60] <- 4
+#d$level[d$playtimes_d30>=60] <- 5
+#mt <- with(d, table(d$level))
+#mt
+#d$logintimes_d30 <- NULL
 d$duration_d30 <- NULL
 cl <- kmeans(scale(d), clusters, iter.max = 20)
 rs <- data.frame(cl$cluster)
@@ -17,15 +26,17 @@ d <- cbind(d, rs)
 paytimes_d30 <- c()
 payamount_d30 <- c()
 playtimes_d30 <- c()
+date_diff <- c()
 total <- c()
 for (i in 1:clusters) {
 	c <- d[which(d$cl.cluster==i),]
 	paytimes_d30 <- append(paytimes_d30, round(mean(c$paytimes_d30), digits=0))
 	payamount_d30 <- append(payamount_d30, round(mean(c$payamount_d30), digits=2))
 	playtimes_d30 <- append(playtimes_d30, round(mean(c$playtimes_d30), digits=2))
+	date_diff <- append(date_diff, round(mean(c$date_diff), digits=2))
 	total <- append(total, mytable[i])
 	print(paste("cluster ", i, " size ", nrow(c)))
 	print(summary(c))}
-out <- data.frame(paytimes_d30, payamount_d30, playtimes_d30, total)
+out <- data.frame(paytimes_d30, payamount_d30, playtimes_d30, date_diff, total)
 print(out)
 write.table(out, "out.txt", sep="\t")
