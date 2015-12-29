727bd1e3f550e745615666c06479ebcbc335c56a
ddmkchan@163.com <ddmkchan@163.com>
2015-12-29 12:41:41 +0800

    fix

--- spider/ranklist_game_merge.py
+++ spider/ranklist_game_merge.py
@@ -85,41 +85,42 @@ def get_game_detail(ranklists):
 	imgs, game_type, summary, download_num, comment_num, rating, pkg_size, author, version, topic_num_total = [u''] * 10
 	for seg in ranklists.split(','):
 		source, identifying = seg.split('^')
-		channel = ranking_2_channel.get(source)
-		_sql =  "select max(dt) as dt, identifying, channel from hot_game_detail_by_day where identifying=\'%s\' and channel=%s group by identifying, channel" % (identifying, channel)
-		rs = db_conn.execute(_sql).fetchone()
-		if rs is not None:
-			dt, identifying, channel = rs
-			ins = db_conn.query(HotGameDetailByDay).filter(HotGameDetailByDay.dt==dt).filter(HotGameDetailByDay.identifying==identifying).filter(HotGameDetailByDay.channel==channel).first()
-			if ins is not None:
-				if not imgs:
-					imgs = ins.imgs
-				if not game_type:
-					game_type = ins.game_type
-				if not summary:
-					summary = ins.summary
-				if not download_num or download_num == u'0':
-					download_num = ins.download_num
-				if not comment_num or comment_num == u'0':
-					comment_num = ins.comment_num
-				if not rating or rating == u'0':
-					rating = ins.rating
-				if not pkg_size:
-					size = ins.pkg_size
-					if ins.pkg_size and u'M' not in ins.pkg_size.upper() and u'G' not in ins.pkg_size.upper():
-						size = "%sM" % round(int(ins.pkg_size)/1024.0/1024.0, 2)
-					pkg_size = size
-				if not author:
-					author = ins.author
-				if not version:
-					version = ins.version
-				if not topic_num_total or topic_num_total == u'0':
-					topic_num_total = ins.topic_num_total
+		channel = ranking_2_channel.get(source, -1)
+		if channel != -1:
+			_sql =  "select max(dt) as dt, identifying, channel from hot_game_detail_by_day where identifying=\'%s\' and channel=%s group by identifying, channel" % (identifying, channel)
+			rs = db_conn.execute(_sql).fetchone()
+			if rs is not None:
+				dt, identifying, channel = rs
+				ins = db_conn.query(HotGameDetailByDay).filter(HotGameDetailByDay.dt==dt).filter(HotGameDetailByDay.identifying==identifying).filter(HotGameDetailByDay.channel==channel).first()
+				if ins is not None:
+					if not imgs:
+						imgs = ins.imgs
+					if not game_type:
+						game_type = ins.game_type
+					if not summary:
+						summary = ins.summary
+					if not download_num or download_num == u'0':
+						download_num = ins.download_num
+					if not comment_num or comment_num == u'0':
+						comment_num = ins.comment_num
+					if not rating or rating == u'0':
+						rating = ins.rating
+					if not pkg_size:
+						size = ins.pkg_size
+						if ins.pkg_size and u'M' not in ins.pkg_size.upper() and u'G' not in ins.pkg_size.upper():
+							size = "%sM" % round(int(ins.pkg_size)/1024.0/1024.0, 2)
+						pkg_size = size
+					if not author:
+						author = ins.author
+					if not version:
+						version = ins.version
+					if not topic_num_total or topic_num_total == u'0':
+						topic_num_total = ins.topic_num_total
 	return (imgs, game_type, summary, download_num, comment_num, rating, pkg_size, author, version, topic_num_total)
 
 
 def get_hot_game_info():
-	for ret in db_conn.query(RankListGame):
+	for ret in db_conn.query(RanklistGame):
 		detail = get_game_detail(ret.ranklists)
 		imgs, game_type, summary, download_num, comment_num, rating, pkg_size, author, version, topic_num_total = detail
 		ins = db_conn.query(RanklistGameDetail).filter(RanklistGameDetail.id==ret.id).first()
@@ -161,5 +162,5 @@ def get_hot_game_info():
 
 		
 if __name__ == '__main__':
-	#hot_games_merge()
+	hot_games_merge()
 	get_hot_game_info()
